8a086409f055997dd64b5b3d9e81e5fc36912499
Wenox <wenoxar@gmail.com>
2020-05-17 17:54:48 +0200

    CMakeLists.txt updated for SFML and TGui

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <TGUI/Gui.hpp>
+
+void example(char const * const src) {
+    char dest[1234];
+    char * pdest3 = dest + 3;
+    std::copy(src, src + 42, pdest3); // C4996
+    std::copy(src, src + 42, dest);   // OK, copy can tell that dest is 1234 elements
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    sf::RenderWindow window({600, 800}, "Title");
+
+    while (window.isOpen()) {
+        sf::Event e;
+        while (window.pollEvent(e)) {
+            if (e.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Cyan);
+        window.display();
+    }
 }
